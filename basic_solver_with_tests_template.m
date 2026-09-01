05ab569cac6e2f348f62ab11a483613044c4f462
Alex Mineeva <amineeva@olin.edu>
2026-09-01 15:19:52 -0400

    Finished bisection solver for part 2

--- basic_solver_with_tests_template.m
+++ basic_solver_with_tests_template.m
@@ -27,14 +27,14 @@ function basic_solver_with_tests_template()
     % plot(x_sol,test_func01(x_sol),'go','markerfacecolor','g','markersize',5);
 
     
-    %Bisection method example test
-    % x_left = -5;
-    % x_right = 2;
-    % plot(x_left,test_func01(x_left),'bo','markerfacecolor','b','markersize',5);
-    % plot(x_right,test_func01(x_right),'ko','markerfacecolor','k','markersize',5);
-    % 
-    % x_sol = bisection_solver(@test_func01,x_left,x_right);
-    % plot(x_sol,test_func01(x_sol),'go','markerfacecolor','g','markersize',5);
+    % Bisection method example test
+    x_left = -5;
+    x_right = 2;
+    plot(x_left,test_func01(x_left),'bo','markerfacecolor','b','markersize',5);
+    plot(x_right,test_func01(x_right),'ko','markerfacecolor','k','markersize',5);
+
+    x_sol = bisection_solver(@test_func01,x_left,x_right);
+    plot(x_sol,test_func01(x_sol),'go','markerfacecolor','g','markersize',5);
 end
 
 
@@ -49,25 +49,37 @@ function [fval,dfdx] = test_func01(x)
 end
 
 
+% fun: the function we are computing the root of
+% x_left: left guess
+% x_right: right guess
+% note that f(x_left) and f(x_right) should have different signs
+%OUTPUTS
+% x: estimate for root of fun
 function x = bisection_solver(fun,x_left,x_right)
-    % find the middle x 
-    x_m = (x_right - x_left)/2
-    % evaluate the function at left, right, and middle x vals
-    f_x_m = fun(x_m)
-    f_x_L = fun(x_left)
-    f_x_R = fun(x_right)
-    % if statements -> determine direction of new bracket
-    if f_x_m = 0
-        %return root
-    elseif (f_x_L > 0 && f_x_m < 0) || (f_x_L < 0 && f_x_m > 0)
-        x_left = x_left
-        x_right = x_m
-    elseif (f_x_L > 0 && f_x_m < 0) || (f_x_L < 0 && f_x_m > 0)
-        
+    f_x_m = 1; % initialize so while loop can start 
+    while (f_x_m ~= 0)
+        % find the middle x 
+        x_m = (x_right + x_left)/2;
+        % evaluate the function at left, right, and middle x vals
+        f_x_m = fun(x_m);
+        f_x_L = fun(x_left);
+        f_x_R = fun(x_right);
+        % if statements -> determine direction of new bracket
+        if f_x_m == 0 %if x_m is root, return root
+            x = x_m;
+            return
+        elseif (f_x_L > 0 && f_x_m < 0) || (f_x_L < 0 && f_x_m > 0)
+            x_left = x_left;
+            x_right = x_m;
+        elseif (f_x_R > 0 && f_x_m < 0) || (f_x_R < 0 && f_x_m > 0)
+            x_right = x_right;
+            x_left = x_m;
+        end
     end
 
-
 end
+
+
 
 %Note that fun(x) should output [f,dfdx], where dfdx is the derivative of f
 function x = newton_solver(fun,x0)
