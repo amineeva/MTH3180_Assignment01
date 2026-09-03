71f8d98473b405d04472b019ebf14fe400d7635e
CatMoonster17 <166651944+CatMoonster17@users.noreply.github.com>
2026-09-03 08:45:49 -0400

    Updated the code to be cleaner

--- newton_solver.m
+++ newton_solver.m
@@ -2,28 +2,28 @@ function x = newton_solver(fun,x0,dxtol,ftol, max_iter,dxmax)
     %Initialize the code
     for i = 1:max_iter
         [f, dfdx] = fun(x0);
-        if abs(f) < ftol
-            x = x0;
-            return
-        end
         %Pulls the value of the function and its derivative at x = x0
-        if (abs(f) == 0)
-        %Checks if the function satisfies the ranged condition
+        if abs(f) < ftol
+            %Ensures the function isn't too small
             x = x0;
             return
-        else
-            if abs(dfdx) <= 1e-14
+        elseif (abs(f) == 0)
+            %Checks if the function satisfies the ranged condition
+            x = x0;
+            return
+        elseif abs(dfdx) <= ftol
                 %Checks if the denominator will be too small
                 x = x0;
                 return
-            end
-            x1 = x0 - (f/dfdx);
-            if abs(x1 - x0) < dxtol || abs(x1-x0) > dxmax
-                x = x0;
-                return
-            else
-                x0 = x1;
-            end
+        end
+        x1 = x0 - (f/dfdx);
+        if abs(x1 - x0) < dxtol || abs(x1-x0) > dxmax
+            x = x0;
+            return
+        else
+            x0 = x1;
+        end
+    end
 
             
 
