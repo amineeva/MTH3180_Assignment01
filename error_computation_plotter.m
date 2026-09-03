d018ee4af7ba830ecd129b361eab2ab0286442ea
Alex Mineeva <amineeva@olin.edu>
2026-09-03 13:29:30 -0400

    Plotter set up for raw error data plot with newton's method

--- error_computation_plotter.m
+++ error_computation_plotter.m
@@ -1,10 +1,13 @@
-% currently just for newton's
 
-% compute errors for newton's solver
+% compute errors for solver of choice -> update line 11
 e_list0 = abs(x_current_list - x_root);
 e_list1 = abs(x_next_list - x_root);
 
 
-
 %example for how to generate a log-log plot
 loglog(e_list0,e_list1,'ro','markerfacecolor','r','markersize',1);
+xlabel('$\epsilon_{n} (-)$', 'Interpreter', 'latex', 'FontSize', 14)
+ylabel('$\epsilon_{n+1} (-)$', 'Interpreter', 'latex', 'FontSize', 14)
+title('Newton''s Method'); % update based on solver
+
+
