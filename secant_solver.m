adb77c421633051d64109efe0bd5cf8066ed3539
CatMoonster17 <166651944+CatMoonster17@users.noreply.github.com>
2026-09-08 15:50:52 -0400

    Streamlined the functions for efficiency

--- secant_solver.m
+++ secant_solver.m
@@ -1,18 +1,18 @@
-function x = secant_solver(fun,x0, x1, dxtol, ftol, max_iter, dxmax)
+function x = secant_solver(fun,x0,x1,dxtol,ftol,max_iter,dxmax)
+    f0 = fun(x0);
+    f1 = fun(x1);
     for i = 1:max_iter
-
         % check denominator to not divide by 0
-        if abs(fun(x1) - fun(x0)) < 1e-14
+        if abs(f1 - f0) < 1e-14
             x = x1;
             return
         end
-
         % calculate the next estimate x
-        x_n = x1 - fun(x1) * (x1 - x0) / (fun(x1) - fun(x0));
-        
+        x_n = x1 - f1 * (x1 - x0) / (f1 - f0);
+
         % evaluate function at new estimate x
-        f_x_n = fun(x_n);
-        if abs(f_x_n) < ftol
+        f_n = fun(x_n);
+        if abs(f_n) < ftol
             x = x_n;
             return
         end
@@ -21,11 +21,13 @@ function x = secant_solver(fun,x0, x1, dxtol, ftol, max_iter, dxmax)
             return
         end
 
-        % if not, update x0 and x1 (guesses)
+        % if not, update x0 and x1 (guesses) and update the functions
         x0 = x1;
+        f0 = f1;
+
         x1 = x_n;
+        f1 = f_n;
     end
 
     x = x_n;
-    return
 end
