5cb1ae984130777a07b8200c1729d866114cde5f
Alex Mineeva <amineeva@olin.edu>
2026-09-08 15:23:42 -0400

    Add discarded list output to bisection solver

--- bisection_solver.m
+++ bisection_solver.m
@@ -1,20 +1,28 @@
-function x = bisection_solver(fun,x_left,x_right, dxtol, ftol, max_iter)
+function [x, discarded_list] = bisection_solver(fun,x_left,x_right, dxtol, ftol, max_iter)
+    discarded_list = [];
+    % Default output
+    x = (x_left + x_right) / 2;
+
+    [fl, dfdx1] = fun(x_left);
+    [fr, dfdx] = fun(x_right);
     % bisection safeguard - check that there is a 0 crossing
-    if (fun(x_left) < 0 && fun(x_right) > 0) || (fun(x_left) > 0 && fun(x_right) < 0)
+    if (fl < 0 && fr > 0) || (fl > 0 && fr < 0)
         for i = 1:max_iter
             % find the middle x 
             x_m = (x_right + x_left)/2;
             % evaluate the function at left, right, and middle x vals
-            f_x_m = fun(x_m);
-            f_x_L = fun(x_left);
-            f_x_R = fun(x_right);
+            [f_x_m, dfdxM] = fun(x_m);
+            [f_x_L, dfdxL] = fun(x_left);
+            [f_x_R, dfdxR] = fun(x_right);
             % if statements -> determine direction of new bracket
             if abs(f_x_m) < ftol %if x_m is root, return root
                 x = x_m;
                 return
             elseif (f_x_L > 0 && f_x_m < 0) || (f_x_L < 0 && f_x_m > 0)
+                discarded_list(end+1) = x_right;
                 x_right = x_m;
             elseif (f_x_R > 0 && f_x_m < 0) || (f_x_R < 0 && f_x_m > 0)
+                discarded_list(end+1) = x_left;
                 x_left = x_m;
             end
 
