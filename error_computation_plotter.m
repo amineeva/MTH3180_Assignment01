6fee0a543659bb447f8b5f649783fa45175a104e
Alex Mineeva <amineeva@olin.edu>
2026-09-08 15:53:52 -0400

    Add method flag, tested and all works

--- error_computation_plotter.m
+++ error_computation_plotter.m
@@ -1,7 +1,32 @@
+%%%%%
+% To get the convergence plots, you must first run 'input_recorder_step_one.m' with your
+% solver of choice, and then run this file with your
+% solver of choice.
+%%%%%
 
-% compute errors for solver of choice -> update line 39, 49, 62
-e_list0 = abs(x_current_list - x_root);
-e_list1 = abs(x_next_list - x_root);
+% % compute errors for solver of choice
+
+if method_flag == 1
+    % newton's method
+    e_list0 = abs(x_current_list - x_true); %used for newton
+    e_list1 = abs(x_next_list - x_true); %used for newton
+    method_name = "Newton's";
+end
+
+if method_flag == 2
+    % bisection
+    e_list0 = abs(x_discarded_list - x_true); % used for bisection
+    e_list1 = abs(x_next_discarded_list - x_true);
+    method_name = "Bisection";
+end
+
+if method_flag == 3
+    % secant
+    e_list0 = abs(x_current_list - x_true);
+    e_list1 = abs(x_next_list - x_true);
+    method_name = "Secant's";
+end
+
 
 
 % Filter the collected error data
@@ -23,43 +48,48 @@ end
 
 [p, k] = generate_error_fit(x_regression,y_regression);
 %generate x data on a logarithmic range
-fit_line_x = 10.^[-16:.01:1];
+fit_line_x = logspace(log10(min(x_regression)), ...
+                      log10(max(x_regression)),200);
 %compute the corresponding y values
 fit_line_y = k*fit_line_x.^p;
 
 %%%% Plotting %%%%
-
-
 %log-log for raw error data
 fig1 = figure(1);
-loglog(e_list0,e_list1,'ro','markerfacecolor','r','markersize',1);
+h1 = loglog(e_list0,e_list1,'ro','markerfacecolor','r','markersize',1);
 axis([1e-18 1e5 1e-18 1e5])
 xlabel('$\epsilon_{n} (-)$', 'Interpreter', 'latex', 'FontSize', 14)
 ylabel('$\epsilon_{n+1} (-)$', 'Interpreter', 'latex', 'FontSize', 14)
-title('Raw Error Data: Newton''s Method'); % update based on solver
+title('Raw Error Data: ' + method_name + ' Method'); % update based on solver
+legend([h1], {'All data'}, 'Location', 'northwest', 'FontSize', 10)
+hold off;
 
 %log-log for filtered error data (overlayed on raw error data
 fig2 = figure(2);
-loglog(e_list0,e_list1,'ro','markerfacecolor','r','markersize',1);
+h1 = loglog(e_list0,e_list1,'ro','markerfacecolor','r','markersize',1);
 hold on;
-loglog(x_regression,y_regression,'b--<','markerfacecolor','r','markersize',1);
+h2 = loglog(x_regression,y_regression,'b.','markersize',1);
 axis([1e-18 1e5 1e-18 1e5])
 xlabel('$\epsilon_{n} (-)$', 'Interpreter', 'latex', 'FontSize', 14)
 ylabel('$\epsilon_{n+1} (-)$', 'Interpreter', 'latex', 'FontSize', 14)
-title('Filtered Error Data: Newton''s Method'); % update based on solver
+title('Filtered Error Data: ' + method_name + ' Method'); % update based on solver
+legend([h1 h2], {'All data', 'Filtered data'}, 'Location', 'northwest', 'FontSize', 10)
 hold off;
 
 %log-log for error data with fit (overlayed on raw error data and filtered
 %data)
 fig3 = figure(3);
-loglog(e_list0,e_list1,'ro','markerfacecolor','r','markersize',1);
+h1 = loglog(e_list0,e_list1,'ro','markerfacecolor','r','markersize',1);
 hold on;
-loglog(x_regression,y_regression,'b--<','markerfacecolor','r','markersize',1);
-loglog(fit_line_x,fit_line_y,'k-','linewidth',2)
+h2 = loglog(x_regression,y_regression,'b.','markersize',1);
+h3 = loglog(fit_line_x,fit_line_y,'k-','linewidth',2);
 axis([1e-18 1e5 1e-18 1e5])
 xlabel('$\epsilon_{n} (-)$', 'Interpreter', 'latex', 'FontSize', 14)
 ylabel('$\epsilon_{n+1} (-)$', 'Interpreter', 'latex', 'FontSize', 14)
-title('Error Data with Fit: Newton''s Method'); % update based on solver
+title('Error Data with Fit: ' + method_name + ' Method'); % update based on solver
+legend([h1 h2 h3], {'All data', 'Filtered data', 'Fit'}, 'Location', 'northwest', 'FontSize', 10)
+subtitle(sprintf('Fit: e_{n+1} = %.4e e_n^{%.4f} | k = %.4e, p = %.4f', k, p, k, p), ...
+    'FontSize', 10);
 hold off;
 
 
