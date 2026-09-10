91e52697514f8fdabc96be35688b1e0e469f00e7
Alex Mineeva <amineeva@olin.edu>
2026-09-10 13:34:02 -0400

    Add exit_flag

--- newton_solver.m
+++ newton_solver.m
@@ -1,15 +1,20 @@
-function x = newton_solver(fun,x0,dxtol,ftol, max_iter,dxmax)
+function [x, exit_flag] = newton_solver(fun,x0,dxtol,ftol, max_iter,dxmax)
+    % exit_flag: success (1), fail (0) -> whether the function finishes
     %Initialize the code
+    exit_flag = 0;
+
     for i = 1:max_iter
         [f, dfdx] = fun(x0);
         %Pulls the value of the function and its derivative at x = x0
         if abs(f) == 0
             %Checks if it is a true root
             x = x0;
+            exit_flag = 1;
             return
         elseif (abs(f) < ftol)
             %Ensures the solution isn't too small
             x = x0;
+            exit_flag = 1;
             return
         elseif abs(dfdx) <= ftol
             %Checks if the denominator will be too small
@@ -18,10 +23,16 @@ function x = newton_solver(fun,x0,dxtol,ftol, max_iter,dxmax)
         end
         x1 = x0 - (f/dfdx);
         %Calculates a new x for the next iterations
-        if abs(x1 - x0) < dxtol || abs(x1-x0) > dxmax
-        %Ensures the calculated step isn't too large or too small
+        if abs(x1 - x0) < dxtol
+            %Ensures the calculated step isn't too small
+            x = x0;
+            exit_flag = 1;
+            return
+        elseif abs(x1-x0) > dxmax
+            %ensures the calculated step isn't too mlarge
             x = x0;
             return
+
         else
             x0 = x1;
             %Establishes new value for x0 for next iteration
