277b81fbe6535772addf0ace218efe357acbade3
Alex Mineeva <amineeva@olin.edu>
2026-09-16 22:30:14 -0400

    For egg plotting it's screaming at me because of the derivative outputs; taking them out made bisection work for finding t_wall and t_ground

--- bisection_solver.m
+++ bisection_solver.m
@@ -6,17 +6,22 @@ function [x, discarded_list, exit_flag] = bisection_solver(fun,x_left,x_right, d
     x = (x_left + x_right) / 2;
     exit_flag = 0;
 
-    [fl, dfdx1] = fun(x_left);
-    [fr, dfdx] = fun(x_right);
+    % [fl, dfdx1] = fun(x_left);
+    fl = fun(x_left);
+    % [fr, dfdx] = fun(x_right);
+    fr = fun(x_right);
     % bisection safeguard - check that there is a 0 crossing
     if (fl < 0 && fr > 0) || (fl > 0 && fr < 0)
         for i = 1:max_iter
             % find the middle x 
             x_m = (x_right + x_left)/2;
             % evaluate the function at left, right, and middle x vals
-            [f_x_m, dfdxM] = fun(x_m);
-            [f_x_L, dfdxL] = fun(x_left);
-            [f_x_R, dfdxR] = fun(x_right);
+            % [f_x_m, dfdxM] = fun(x_m);
+            % [f_x_L, dfdxL] = fun(x_left);
+            % [f_x_R, dfdxR] = fun(x_right);
+            f_x_m = fun(x_m);
+            f_x_L = fun(x_left);
+            f_x_R = fun(x_right);
             % if statements -> determine direction of new bracket
             if abs(f_x_m) < ftol %if x_m is root, return root
                 x = x_m;
