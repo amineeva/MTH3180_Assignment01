8603733e283a083f6633b3f6acbd185eb665b5ab
CatMoonster17 <166651944+CatMoonster17@users.noreply.github.com>
2026-09-20 18:07:10 -0400

    Added exit flags, removed redundant code

--- solvers/basic_solver_with_tests_template.m
+++ solvers/basic_solver_with_tests_template.m
@@ -18,7 +18,7 @@ function basic_solver_with_tests_template()
     % %Newton's method example test
     % x0_guess = 2;
     % plot(x0_guess,test_func01(x0_guess),'bo','markerfacecolor','b','markersize',5); 
-    % x_sol = newton_solver(@test_func01,x0_guess, dxtol, ftol, max_iter, dxmax);
+    % [x_sol, exit_flag] = newton_solver(@test_func01,x0_guess, dxtol, ftol, max_iter, dxmax);
     % plot(x_sol,test_func01(x_sol),'go','markerfacecolor','g','markersize',5);
     
 
@@ -28,7 +28,7 @@ function basic_solver_with_tests_template()
     % plot(x0_guess,test_func01(x0_guess),'bo','markerfacecolor','b','markersize',5);
     % plot(x1_guess,test_func01(x1_guess),'ko','markerfacecolor','k','markersize',5);
     % 
-    % x_sol = secant_solver(@test_func01,x0_guess,x1_guess, dxtol, ftol, max_iter, dxmax);
+    % [x_sol, exit_flag] = secant_solver(@test_func01,x0_guess,x1_guess, dxtol, ftol, max_iter, dxmax);
     % plot(x_sol,test_func01(x_sol),'go','markerfacecolor','g','markersize',5);
 
 
@@ -38,7 +38,7 @@ function basic_solver_with_tests_template()
     % plot(x_left,test_func01(x_left),'bo','markerfacecolor','b','markersize',5);
     % plot(x_right,test_func01(x_right),'ko','markerfacecolor','k','markersize',5);
     % 
-    % x_sol = bisection_solver(@test_func01,x_left,x_right, dxtol, ftol, max_iter);
+    % [x_sol, exit_flag] = bisection_solver(@test_func01,x_left,x_right, dxtol, ftol, max_iter);
     % plot(x_sol,test_func01(x_sol),'go','markerfacecolor','g','markersize',5);
 
 end
@@ -94,95 +94,6 @@ function x = bisection_solver(fun,x_left,x_right, dxtol, ftol, max_iter)
         x = (x_left + x_right) / 2;
     end
 end
-
-
-
-%Note that fun(x) should output [f,dfdx], where dfdx is the derivative of f
-%INPUTS:
-% fun: the function we are computing the root of
-% Note that fun(x) should output [f,dfdx], where dfdx is the derivative of f
-% (see test_func01 below for example)
-% x0: initial guess for Newton's method
-% dxtol: termination threshold (stop when interval abs(x_{i+1}-x_i) < dxtol)
-% ftol: termination threshold (stop when abs(f(x_{i}))<ftol
-% max_iter: maximum iteration limit
-% dxmax: threshold for checking for a divide by zero error:
-% terminate when abs(x_{i+1}-x_i) > dxmax, where dxmax is a very large number
-%OUTPUTS
-% x: estimate for root of fun
-% exit_flag: an integer indicating whether or not the solver succeeded
-function x = newton_solver(fun,x0,dxtol,ftol, max_iter,dxmax)
-    %Initialize the code
-    for i = 1:max_iter
-        [f, dfdx] = fun(x0);
-        if abs(f) < ftol
-            x = x0;
-            return
-        end
-        %Pulls the value of the function and its derivative at x = x0
-        if (abs(f) == 0)
-        %Checks if the function satisfies the ranged condition
-            x = x0;
-            return
-        else
-            if abs(dfdx) <= 1e-14
-                %Checks if the denominator will be too small
-                x = x0;
-                return
-            end
-            x1 = x0 - (f/dfdx);
-            if abs(x1 - x0) < dxtol || abs(x1-x0) > dxmax
-                x = x0;
-                return
-            else
-                x0 = x1;
-            end
-
-            
-
-            %Establishes new value for x0 for next iteration
-        end
-    end
-end
-
-
-%Root finding function via secant method
-%INPUTS:
-% fun: the function we are computing the root of
-% x0: first guess for secant method
-% x1: second guess for secant method
-% max_iter: maximum iteration limit
-% dxmax: threshold for checking for a divide by zero error:
-% terminate when abs(x_{i+1}-x_i) > dxmax, where dxmax is a very large number
-%OUTPUTS
-% x: estimate for root of fun
-function x = secant_solver(fun,x0, x1, dxtol, ftol, max_iter, dxmax)
-    for i = 1:max_iter
-
-        % check denominator to not divide by 0
-        if abs(fun(x1) - fun(x0)) < 1e-14
-            x = x1;
-            return
-        end
-
-        % calculate the next estimate x
-        x_n = x1 - fun(x1) * (x1 - x0) / (fun(x1) - fun(x0));
-        
-        % evaluate function at new estimate x
-        f_x_n = fun(x_n);
-        if abs(f_x_n) < ftol
-            x = x_n;
-            return
-        end
-        if abs(x_n - x1) < dxtol
-            x = x_n;
-            return
-        end
-
-        % if not, update x0 and x1 (guesses)
-        x0 = x1;
-        x1 = x_n;
-    end
 
     x = x_n;
     return
