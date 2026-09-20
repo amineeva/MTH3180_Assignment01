d55566054a35df5d17ab1647ead325804ec698a3
CatMoonster17 <166651944+CatMoonster17@users.noreply.github.com>
2026-09-20 18:08:07 -0400

    Added exit flag
    Refactor secant_solver to return exit_flag and improve checks.

--- solvers/secant_solver.m
+++ solvers/secant_solver.m
@@ -1,27 +1,51 @@
-function x = secant_solver(fun,x0,x1,dxtol,ftol,max_iter,dxmax)
+function [x, exit_flag] = secant_solver(fun,x0,x1,dxtol,ftol,max_iter,dxmax)
+
+    exit_flag = 0;
+
     f0 = fun(x0);
     f1 = fun(x1);
+
     for i = 1:max_iter
-        % check denominator to not divide by 0
-        if abs(f1 - f0) < 1e-14
+
+        % Check denominator
+        if abs(f1 - f0) < eps * max([1, abs(f1), abs(f0)])
             x = x1;
             return
         end
-        % calculate the next estimate x
+
+        % Calculate next estimate
         x_n = x1 - f1 * (x1 - x0) / (f1 - f0);
 
-        % evaluate function at new estimate x
-        f_n = fun(x_n);
-        if abs(f_n) < ftol
-            x = x_n;
-            return
-        end
-        if abs(x_n - x1) < dxtol
+        % Check for numerical failure
+        if ~isfinite(x_n)
             x = x_n;
             return
         end
 
-        % if not, update x0 and x1 (guesses) and update the functions
+        % Check maximum step
+        if abs(x_n - x1) > dxmax
+            x = x_n;
+            return
+        end
+
+        % Evaluate function
+        f_n = fun(x_n);
+
+        % Check function convergence
+        if abs(f_n) < ftol
+            x = x_n;
+            exit_flag = 1;
+            return
+        end
+
+        % Check x convergence
+        if abs(x_n - x1) < dxtol
+            x = x_n;
+            exit_flag = 1;
+            return
+        end
+
+        % Update
         x0 = x1;
         f0 = f1;
 
