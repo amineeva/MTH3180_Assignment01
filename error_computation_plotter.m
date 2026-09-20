8965f82405e9c5a102b074d17f9f3ae88bdc3ea4
Alex Mineeva <amineeva@olin.edu>
2026-09-20 13:56:37 -0400

    Applied Orion's suggested changes to convergence plots

--- error_computation_plotter.m
+++ error_computation_plotter.m
@@ -1,5 +1,5 @@
 %%%%%
-% To get the convergence plots, you must first run 'input_recorder_step_one.m' with your
+% To get the convergence plots, you must first run 'input_recorder_test.m' with your
 % solver of choice, and then run this file with your
 % solver of choice.
 %%%%%
@@ -27,6 +27,13 @@ if method_flag == 3
     method_name = "Secant's";
 end
 
+if method_flag == 4
+    % fzero
+    e_list0 = abs(x_current_list - x_true);
+    e_list1 = abs(x_next_list - x_true);
+    method_name = "Fzero";
+end
+
 
 
 % Filter the collected error data
@@ -48,48 +55,47 @@ end
 
 [p, k] = generate_error_fit(x_regression,y_regression);
 %generate x data on a logarithmic range
-fit_line_x = logspace(log10(min(x_regression)), ...
-                      log10(max(x_regression)),200);
+fit_line_x = logspace(log10(min(x_regression)/10000000), ...
+                      log10(max(x_regression)*1000000000),200);
 %compute the corresponding y values
 fit_line_y = k*fit_line_x.^p;
 
 %%%% Plotting %%%%
 %log-log for raw error data
 fig1 = figure(1);
-h1 = loglog(e_list0,e_list1,'ro','markerfacecolor','r','markersize',1);
+h1 = loglog(e_list0,e_list1,'r.','markerfacecolor','r','markersize',4);
 axis([1e-18 1e5 1e-18 1e5])
-xlabel('$\epsilon_{n} (-)$', 'Interpreter', 'latex', 'FontSize', 14)
-ylabel('$\epsilon_{n+1} (-)$', 'Interpreter', 'latex', 'FontSize', 14)
-title('Raw Error Data: ' + method_name + ' Method'); % update based on solver
-legend([h1], {'All data'}, 'Location', 'northwest', 'FontSize', 10)
+xlabel('Error at current iteration (-)', 'FontSize', 16);
+ylabel('Error at next iteration (-)', 'FontSize', 16);
+title('Error Convergence of ' + method_name + ' Method', 'FontSize', 17);
+legend([h1], {'All data'}, 'Location', 'northwest', 'FontSize', 12);
 hold off;
 
 %log-log for filtered error data (overlayed on raw error data
 fig2 = figure(2);
-h1 = loglog(e_list0,e_list1,'ro','markerfacecolor','r','markersize',1);
+h1 = loglog(e_list0,e_list1,'r.','markerfacecolor','r','markersize',4);
 hold on;
-h2 = loglog(x_regression,y_regression,'b.','markersize',1);
+h2 = loglog(x_regression,y_regression,'b.','markersize',4);
 axis([1e-18 1e5 1e-18 1e5])
-xlabel('$\epsilon_{n} (-)$', 'Interpreter', 'latex', 'FontSize', 14)
-ylabel('$\epsilon_{n+1} (-)$', 'Interpreter', 'latex', 'FontSize', 14)
-title('Filtered Error Data: ' + method_name + ' Method'); % update based on solver
-legend([h1 h2], {'All data', 'Filtered data'}, 'Location', 'northwest', 'FontSize', 10)
+xlabel('Error at current iteration (-)', 'FontSize', 16);
+ylabel('Error at next iteration (-)', 'FontSize', 16);
+title('Filtered Error Convergence of ' + method_name + ' Method', 'FontSize', 17);
+legend([h1 h2], {'All data', 'Filtered data'}, 'Location', 'northwest', 'FontSize', 12);
 hold off;
 
 %log-log for error data with fit (overlayed on raw error data and filtered
 %data)
 fig3 = figure(3);
-h1 = loglog(e_list0,e_list1,'ro','markerfacecolor','r','markersize',1);
+h1 = loglog(e_list0,e_list1,'r.','markerfacecolor','r','markersize',4);
 hold on;
-h2 = loglog(x_regression,y_regression,'b.','markersize',1);
-h3 = loglog(fit_line_x,fit_line_y,'k-','linewidth',2);
+h2 = loglog(x_regression,y_regression,'b.','markersize',4);
+h3 = loglog(fit_line_x,fit_line_y,'k-','linewidth',1);
 axis([1e-18 1e5 1e-18 1e5])
-xlabel('$\epsilon_{n} (-)$', 'Interpreter', 'latex', 'FontSize', 14)
-ylabel('$\epsilon_{n+1} (-)$', 'Interpreter', 'latex', 'FontSize', 14)
-title('Error Data with Fit: ' + method_name + ' Method'); % update based on solver
-legend([h1 h2 h3], {'All data', 'Filtered data', 'Fit'}, 'Location', 'northwest', 'FontSize', 10)
-subtitle(sprintf('Fit: e_{n+1} = %.4e e_n^{%.4f} | k = %.4e, p = %.4f', k, p, k, p), ...
-    'FontSize', 10);
+xlabel('Error at current iteration (-)', 'FontSize', 16)
+ylabel('Error at next iteration (-)', 'FontSize', 16)
+title('Error Convergence of ' + method_name + ' Method with Fit', 'FontSize', 17);
+legend([h1 h2 h3], {'All data', 'Filtered data', 'Fit'}, 'Location', 'northwest', 'FontSize', 12)
+% subtitle(sprintf('Fit: e_{n+1} = %.4e e_n^{%.4f} | k = %.4e, p = %.4f', k, p, k, p), 'FontSize', 12);
 hold off;
 
 
@@ -100,9 +106,12 @@ hold off;
 %y_regression -> e_{n+1}
 %p and k are the output coefficients
 function [p,k] = generate_error_fit(x_regression,y_regression)
+    x_regression = x_regression(:);
+    y_regression = y_regression(:);
+
     %generate Y, X1, and X2
-    Y = log(y_regression)';
-    X1 = log(x_regression)';
+    Y = log(y_regression); %';
+    X1 = log(x_regression); %';
     X2 = ones(length(X1),1);
     %run the regression
     coeff_vec = regress(Y,[X1,X2]);
